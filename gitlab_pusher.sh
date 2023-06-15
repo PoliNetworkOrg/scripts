0a3fc4289793259328fdcb3ec61d68a15593c7fb
Elia Maggioni <elia.maggioni@gmail.com>
2023-06-15 18:37:58 +0200

    Create gitlab_pusher.sh

---
+++ gitlab_pusher.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [ -z "$1" ]
+then
+  echo -e "${RED}No password supplied, terminating script.${NC}"
+  exit 1
+fi
+
+PASSWORD=$1
+
+courses=( aes,aero1y,aero2y,AES1y,AES2y,AES3y,AES_corsi6cfu,space1y,space2y automazione,automazione1y,automazione2y,automazione3y chimica,chimica1y,chimica2y,chimica3y info,info1y,info2y,info3y,CompSc,CompSc1,CompSc2 electronics,elettronica1y,elettronica2y,elettronica3y energetica,energetica1y,energetica2y,energetica3y matnano,matnano1y,matnano2y,matnano3y meccanica,meccanica1y,meccanica2y,meccanica3y mobilitymd,mobilityMDy1,mobilityMDy2 bioinformatics,Bioinformatics management,Gest1y,Gest2y,Gest3y,Manag1y,Manag2y mechanical,Mech1y,Mech2y,Mech3y design,DES-Moda,DES-Interni,DES-Comm,DES-Prod )
+
+echo -e "${RED}Starting push to all repos ${NC}"
+
+for i in "${courses[@]}"
+do
+   IFS="," read -r -a arr <<< "${i}"
+   cd ${arr[0]}
+   for e in "${arr[@]:1}"
+   do
+     cd $e
+     echo -e "${RED}pushing for $e ${NC}"
+     git push https://polibot:"$PASSWORD"@gitlab.com/polinetwork/$e.git --all
+     cd ..
+   done
+   cd ..
+done
+
+echo -e "${RED}All repos pushed ${NC}"
