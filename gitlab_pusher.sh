e56ba5209d3205ce33a09676b647536a663fb6a5
Elia Maggioni <elia.maggioni@gmail.com>
2023-06-15 18:54:50 +0200

    Update gitlab_pusher.sh

--- gitlab_pusher.sh
+++ gitlab_pusher.sh
@@ -11,7 +11,7 @@ fi
 
 PASSWORD=$1
 
-courses=( aes,aero1y,aero2y,AES1y,AES2y,AES3y,AES_corsi6cfu,space1y,space2y automazione,automazione1y,automazione2y,automazione3y chimica,chimica1y,chimica2y,chimica3y info,info1y,info2y,info3y,CompSc,CompSc1,CompSc2 electronics,elettronica1y,elettronica2y,elettronica3y energetica,energetica1y,energetica2y,energetica3y matnano,matnano1y,matnano2y,matnano3y meccanica,meccanica1y,meccanica2y,meccanica3y mobilitymd,mobilityMDy1,mobilityMDy2 bioinformatics,Bioinformatics management,Gest1y,Gest2y,Gest3y,Manag1y,Manag2y mechanical,Mech1y,Mech2y,Mech3y design,DES-Moda,DES-Interni,DES-Comm,DES-Prod )
+courses=( aes,aero1y,aero2y,AES1y,AES2y,AES3y,AES_corsi6cfu,space1y,space2y automazione,automazione1y,automazione2y,automazione3y chimica,chimica1y,chimica2y,chimica3y info,info1y,info2y,info3y,CompSc,CompSc1,CompSc2 electronics,elettronica1y,elettronica2y,elettronica3y energetica,energetica1y,energetica2y,energetica3y matnano,matnano1y,matnano2y,matnano3y meccanica,meccanica1y,meccanica2y,meccanica3y mobilitymd,mobilityMDy1,mobilityMDy2 bioinformatics,Bioinformatics management,Gest1y,Gest2y,Gest3y,Manag1y,Manag2y mechanical,Mech1y,Mech2y design,DES-Moda,DES-Interni,DES-Comm,DES-Prod )
 
 echo -e "${RED}Starting push to all repos ${NC}"
 
