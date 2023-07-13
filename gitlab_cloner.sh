e443fc2feac4745a188a428863ceb559565dac22
Elia Maggioni <elia.maggioni@gmail.com>
2023-07-13 23:45:45 +0200

    Update gitlab_cloner.sh

--- gitlab_cloner.sh
+++ gitlab_cloner.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
-courses=( aes,aero1y,aero2y,AES1y,AES2y,AES3y,AES_corsi6cfu,space1y,space2y automazione,automazione1y,automazione2y,automazione3y chimica,chimica1y,chimica2y,chimica3y info,info1y,info2y,info3y,CompSc,CompSc1,CompSc2 electronics,elettronica1y,elettronica2y,elettronica3y energetica,energetica1y,energetica2y,energetica3y matnano,matnano1y,matnano2y,matnano3y meccanica,meccanica1y,meccanica2y,meccanica3y mobilitymd,mobilityMDy1,mobilityMDy2 bioinformatics,Bioinformatics management,Gest1y,Gest2y,Gest3y,Manag1y,Manag2y mechanical,Mech1y,Mech2y design,DES-Moda,DES-Interni,DES-Comm,DES-Prod )
+courses=( aes,aero1y,aero2y,AES1y,AES2y,AES3y,AES_corsi6cfu,space1y,space2y automazione,automazione1y,automazione2y,automazione3y chimica,chimica1y,chimica2y,chimica3y info,info1y,info2y,info3y,CompSc,CompSc1,CompSc2 electronics,elettronica1y,elettronica2y,elettronica3y energetica,energetica1y,energetica2y,energetica3y matnano,matnano1y,matnano2y,matnano3y meccanica,meccanica1y,meccanica2y,meccanica3y mobilitymd,mobilityMDy1,mobilityMDy2 bioinformatics,Bioinformatics management,Gest1y,Gest2y,Gest3y,Manag1y,Manag2y mechanical,Mech1y,Mech2y design,DES-Moda,DES-Interni,DES-Comm,DES-Prod ambientale,ambientale1y,ambientale2y,ambientale3y )
 
 echo -e "${RED}starting work ${NC}"
 for i in "${courses[@]}"
